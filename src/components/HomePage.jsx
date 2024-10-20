1a37a4a551074c49783880d6fb5563a812e3c2a5
Kubilay ADA <kubilayada.26@gmail.com>
2024-10-20 17:41:03 +0200

    fix alt

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -26,28 +26,28 @@ const HomePage = () => {
             <div className="info-container">
                 <div className="info-box" onClick={() => handleInfoBoxClick('/about')}>
                     <h2>About DRINK IT!</h2>
-                    <img src={bottleImage} alt="A reusable bottle" className="info-image" />
+                    <img src={bottleImage} alt="A reusable bottle showcasing eco-friendly hydration" className="info-image" />
                     <p>
                         DRINK IT! is committed to reducing plastic waste and promoting sustainable consumption through our innovative refill stations and futuristic water bottle.
                     </p>
                 </div>
                 <div className="info-box" onClick={() => handleInfoBoxClick('/how-it-works')}>
                     <h2>How It Works</h2>
-                    <img src={partnerImage} alt="partner image" className="info-image" />
+                    <img src={partnerImage} alt="Partner locations for refill stations" className="info-image" />
                     <p>
                         Simply bring your reusable bottle to one of our refill stations (partner restaurants, cafes, or bars), choose your drink, and refill it. It's that easy!
                     </p>
                 </div>
                 <div className="info-box" onClick={() => handleInfoBoxClick('/signup')}>
                     <h2>Join the Movement</h2>
-                    <img src={poolImage} alt="movement" className="info-image" />
+                    <img src={poolImage} alt="Join the DRINK IT! movement" className="info-image" />
                     <p>
                         Sign up for exclusive offers, new refill station locations, and tips on how to lead a sustainable lifestyle. Let’s DRINK IT together!
                     </p>
                 </div>
                 <div className="info-box" onClick={() => handleInfoBoxClick('/sustainability')}>
                     <h2>Sustainability Commitment</h2>
-                    <img src={saveImage} alt="save" className="info-image" />
+                    <img src={saveImage} alt="Sustainable practices for a cleaner planet" className="info-image" />
                     <p>
                         We prioritize eco-friendly practices in our operations to ensure a cleaner planet for future generations.
                     </p>

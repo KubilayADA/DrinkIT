214fc42b7b32211376d5473e62f7f7bd7c6d05fa
Kubilay ADA <kubilayada.26@gmail.com>
2024-12-04 12:06:34 +0100

    broke a lit

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import '../styles/HomePage.css';
-import Navbar from './Navbar';
+import Navbar from './Navbar'; 
+import { useNavigate } from 'react-router-dom'; 
 
 const HomePage = () => {
+    const navigate = useNavigate(); 
+    
+    // Removed unused 'partnerImage', 'poolImage', 'saveImage', and 'handleInfoBoxClick' to comply with ESLint rules
+
     return (
         <div className="homepage">
-            {/* Navigation Bar */}
             <Navbar />
-
-            {/* Header Section */}
             <header className="homepage-header">
                 <img src="src/assets/logo.png" alt="Drink It logo" className="navbar-logo" />
             </header>
 
-            {/* Footer Section */}
-            <footer className="homepage-footer">
+            <footer>
                 <p>&copy; 2024 DRINK IT! All rights reserved. By DRINK IT! CODE Berlin</p>
             </footer>
         </div>
@@ -22,4 +23,3 @@ const HomePage = () => {
 };
 
 export default HomePage;
-git 
